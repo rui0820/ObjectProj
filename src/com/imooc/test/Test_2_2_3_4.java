bcf2e7831673bc320f2dd40cff0fc9a8f818d223
rui0820 <karaku0820@gamil.com>
2019-08-18 17:15:30 +0800

    第二次提交代码

---
+++ src/com/imooc/test/Test_2_2_3_4.java
@@ -0,0 +1,8 @@
+package com.imooc.test;
+
+public class Test_2_2_3_4 {
+	private static float[] f = new float[2];
+	public static void main(String[] args) {
+		System.out.println("f[0] = " + f[0]);
+	}
+}
